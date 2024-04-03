113dedec61319a24c8d623d3349dbac1967fb996
Md-Neamul-Hoqe <hoqe1997@gmail.com>
2024-04-03 21:08:03 +0600

    form created with name, email and phone field

---
+++ src/app/ui/page.js
@@ -0,0 +1,38 @@
+'use client'
+
+import InputField from '@/components/input';
+import InputLabel from '@/components/label';
+import { useForm } from 'react-hook-form';
+
+export default function FormAside() {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors }
+    } = useForm();
+
+    const formDataSubmit = data => {
+        // const form = new FormData()
+        // const name = form.get('name')
+        console.log('data on the form: ', data);
+    }
+    return (
+        <form className='flex flex-col gap-5 card-body' onSubmit={handleSubmit(formDataSubmit)}>
+            <div className="form-control">
+                <InputLabel label="Name" />
+                <InputField type='text' register={register} errors={errors} placeholder='Your name...' name='name' />
+            </div>
+            <div className="form-control">
+                <InputLabel label='Email' />
+                <InputField register={register} errors={errors} name='email' type='email' options={{ required: true }} placeholder='yourname@provider.com' />
+            </div>
+            <div className="form-control">
+                <InputLabel label='Phone' />
+                <InputField register={register} errors={errors} name='phone' type='tel' placeholder='+880 17XXX XX XX' />
+            </div>
+            <div className='flex justify-end'>
+                <button className='btn' type='submit'>Submit now</button>
+            </div>
+        </form>
+    );
+}
